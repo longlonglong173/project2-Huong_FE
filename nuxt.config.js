b2c87ee43950b9f5d5e049268b020871bfd37547
longlonglong173 <71415818+longlonglong173@users.noreply.github.com>
2022-01-22 16:12:45 +0700

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,59 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Travel',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      {
+        src: 'https://unpkg.com/swiper/swiper-bundle.min.js',
+        body: true,
+      },
+      {
+        src: 'https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700&display=swap',
+      },
+    ],
+  },
+  // ssr: false,
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/styles/resetcss.css',
+    '~/assets/styles/style.css',
+    '~/assets/styles/fontawesome/css/all.css',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
